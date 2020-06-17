6471ab0e4306d58e51c26b17320896b6764e7ff1
BatoolAliHussain <66515156+BatoolAliHu@users.noreply.github.com>
2020-06-17 20:33:02 +0300

    done
    cw2-p1,p2,p3,p4

---
+++ ios -cw- p1.playground/Contents.swift
@@ -0,0 +1,21 @@
+
+
+let family = [ "Hussain", "Abyar", "Ismail", "Zainab", "Batool"]
+
+var evens = [2, 4, 6, 8, 10, 12]
+
+var grades = [90.9, 93.2, 93.4]
+
+var trueFalse = [true, true, true, false]
+
+print(family[0], family[4])
+
+print(evens[0], evens[5] )
+
+print(grades[0], grades[2])
+
+print(trueFalse[0], trueFalse[3])
+
+
+
+
